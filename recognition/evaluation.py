bb75eb71d13aad3f0f6186ed31ae3467e809ce28
NNNguyenDuyyy <122520891+NNNguyenDuyyy@users.noreply.github.com>
2024-07-13 19:51:25 +0700

    Update evaluation.py

--- recognition/evaluation.py
+++ recognition/evaluation.py
@@ -12,40 +12,40 @@ from tests.testkits import kface1v1verification, face1v1verification, ijb1v1veri
 
 
 def evals(data_cfg, save_dir, model, datasets, device, wname='last', train=False, re_eval=False):    
-   cfg = data_cfg['test']
-   if cfg['dataset'] == 'kface':
-       save_feat_dir = save_dir / 'kface'
-       save_feat_dir.mkdir(parents=True, exist_ok=True)  # make dir
-       df_path = save_feat_dir / (wname + '_deepfeatures.pth')                   
-       acc = kface1v1verification(cfg['test_pairs_txt'], df_path, model, datasets.loader, device, metric=cfg['metric'], train=train)
+    cfg = data_cfg['test']
+    if cfg['dataset'] == 'kface':
+        save_feat_dir = save_dir / 'kface'
+        save_feat_dir.mkdir(parents=True, exist_ok=True)  # make dir
+        df_path = save_feat_dir / (wname + '_deepfeatures.pth')                   
+        acc = kface1v1verification(cfg['test_pairs_txt'], df_path, model, datasets.loader, device, metric=cfg['metric'], train=train)
 
-   elif cfg['dataset'] == 'bin':                
-      if isinstance(datasets, list):
-           for dataset in datasets:         
-               print("Test data_path: ", dataset.data_path)       
-               acc = face1v1verification(model, dataset.loader, device, cfg['metric'])
-       else:
-           acc = face1v1verification(model, datasets.loader, device, cfg['metric'])
-      
-   elif cfg['dataset'] in ['ijbb', 'ijbc']:
-       save_feat_dir = save_dir / cfg['dataset'] 
-       save_feat_dir.mkdir(parents=True, exist_ok=True)  # make dir
-       df_path = save_feat_dir / (wname + '.feat.list')        
-       media_txt = Path(cfg['root']) / cfg['media_txt']
-       pair_label_txt = Path(cfg['root']) / cfg['pair_label_txt']
-       acc = ijb1v1verification(model, datasets.loader, df_path, media_txt,
-                                pair_label_txt, device=device, metric=cfg['metric'])
+    elif cfg['dataset'] == 'bin':                
+        if isinstance(datasets, list):
+            for dataset in datasets:         
+                print("Test data_path: ", dataset.data_path)       
+                acc = face1v1verification(model, dataset.loader, device, cfg['metric'])
+        else:
+            acc = face1v1verification(model, datasets.loader, device, cfg['metric'])
+        
+    elif cfg['dataset'] in ['ijbb', 'ijbc']:
+        save_feat_dir = save_dir / cfg['dataset'] 
+        save_feat_dir.mkdir(parents=True, exist_ok=True)  # make dir
+        df_path = save_feat_dir / (wname + '.feat.list')        
+        media_txt = Path(cfg['root']) / cfg['media_txt']
+        pair_label_txt = Path(cfg['root']) / cfg['pair_label_txt']
+        acc = ijb1v1verification(model, datasets.loader, df_path, media_txt,
+                                 pair_label_txt, device=device, metric=cfg['metric'])
 
-   elif cfg['dataset'] == 'megface':
-       acc = 0 # we will update later
+    elif cfg['dataset'] == 'megface':
+        acc = 0 # we will update later
     
-   return acc
+    return acc
 
     
 def inference(opt, device):
     save_dir = Path(opt.save_dir)
     with open(opt.data) as f:        
-       data_cfg = yaml.load(f, Loader=yaml.FullLoader)
+        data_cfg = yaml.load(f, Loader=yaml.FullLoader)
     data_name = opt.data.split('/')[-1]    
     sys.stdout = Logger(save_dir / 'test_log_{}.{}.txt'.format(opt.wname, data_name))
 
@@ -59,7 +59,6 @@ def inference(opt, device):
     
     datasets = build_datasets(data_cfg, opt.batch_size, cuda, opt.workers, mode='test')
     evals(data_cfg, save_dir, model, datasets, device, opt.wname, train=False)
-    #return model
     
 def parser():    
     parser = argparse.ArgumentParser(description='Face Test')
@@ -92,5 +91,3 @@ if __name__ == "__main__":
         
     device = select_device(opt.device, batch_size=opt.batch_size, rank=opt.global_rank)
     inference(opt, device)
-    # model = inference(opt, device)
-    # print(model)
